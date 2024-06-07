d9bcb3bdebbb7e2ba426fb0df75099793896c2df
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-07 23:16:49 +0200

    more changes

--- extension_config.cmake
+++ extension_config.cmake
@@ -5,10 +5,3 @@ duckdb_extension_load(delta
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
-
-# Build the httpfs extension to test with s3/http
-duckdb_extension_load(httpfs)
-
-# Build the tpch and tpcds extension for testing/benchmarking
-duckdb_extension_load(tpch)
-duckdb_extension_load(tpcds)
